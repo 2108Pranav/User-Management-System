73b16cfa1de178ed527e167bfa58f422f6d8883b
Tirth Patel <tirthpatel42722@gmail.com>
2026-05-30 13:16:10 +0530

    db initialization

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
+from db import *
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello world!"
+
+# initialize db
+initialized_db(app)
---
+++ db.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config.settings import Config
+mysql_db = SQLAlchemy()
+
+db_string = f"mysql://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}/{Config.DB_NAME}"
+print(db_string)
+
+def initialized_db(app):
+    """Use this function for initializing db."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_string
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
+    mysql_db.init_app(app)
+
+    Migrate(app, mysql_db)
+
+    @app.before_request
+    def create_tables():
+        app.before_request_funcs[None].remove(create_tables)
+        mysql_db.create_all()
